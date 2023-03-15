293fd08b89edcf0e8ee48fa25be180aa8c73dbd3
navsingh3211 <navsingh3211@gmail.com>
2023-03-16 00:02:50 +0530

    creating build and pusing it

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ const DB = require("./db/config");
 require("dotenv").config();
 
 const User = require("./db/User");
+const path = require("path");
 
 // console.log(process.env);
 const PORT=process.env.PORT;
 const app = express();
 
+
 DB.connectDB();
 
 app.use(express.json());
 app.use(cors());
+
+// Code block to serve frontend from server : 
+// (Replace client with your frontend folder name)
+//start-->
+app.use(express.static(path.join(__dirname, "./front-end/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./front-end/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
+//end<---
 
 app.post("/register", async (req, res) => {
     let user = new User(req.body);
